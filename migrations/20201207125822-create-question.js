4a1cb909ef8268ec3d2f5cb890c66c4ffe05f7bd
RDOToole89 <roibinotoole@gmail.com>
2020-12-07 14:00:29 +0100

    Create question model

---
+++ migrations/20201207125822-create-question.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      body: {
+        type: Sequelize.TEXT
+      },
+      authorId: {
+        type: Sequelize.INTEGER
+      },
+      questionBoardsId: {
+        type: Sequelize.INTEGER
+      },
+      upVotes: {
+        type: Sequelize.INTEGER
+      },
+      resolved: {
+        type: Sequelize.BOOLEAN
+      },
+      solverBy: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('questions');
+  }
+};
---
+++ models/question.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class question extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  question.init({
+    title: DataTypes.STRING,
+    body: DataTypes.TEXT,
+    authorId: DataTypes.INTEGER,
+    questionBoardsId: DataTypes.INTEGER,
+    upVotes: DataTypes.INTEGER,
+    resolved: DataTypes.BOOLEAN,
+    solverBy: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'question',
+  });
+  return question;
+};
