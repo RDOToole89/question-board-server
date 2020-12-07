5037819c540c48994ddad40bce6f4d3418035a68
RDOToole89 <roibinotoole@gmail.com>
2020-12-07 14:26:14 +0100

    Setup basic express APP

---
+++ index.js
@@ -0,0 +1,20 @@
+const cors = require("cors");
+const express = require("express");
+const { cloudinary } = require("./config/cloudinary");
+const authRouter = require("./routers/authorization");
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// MiddleWares
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+
+app.get("/", (req, res, next) => {
+  res.json("Hello World");
+});
+
+// Routers
+app.use("/", authRouter);
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
