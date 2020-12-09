a46a8b1d9f3e5f263de342fbca55e924d48142ac
RDOToole89 <roibinotoole@gmail.com>
2020-12-09 12:37:59 +0100

    Fix bug in question model, create ROUTE to GET single question

--- migrations/20201207130450-create-question.js
+++ migrations/20201207130450-create-question.js
@@ -1,63 +1,63 @@
-"use strict";
+'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("questions", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      title: {
-        type: Sequelize.STRING,
-      },
-      body: {
-        type: Sequelize.TEXT,
-      },
-      authorId: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: "users",
-          key: "id",
-        },
-        onUpdate: "CASCADE",
-        onDelete: "SET NULL",
-      },
-      questionBoardId: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: "questionBoards",
-          key: "id",
-        },
-        onUpdate: "CASCADE",
-        onDelete: "SET NULL",
-      },
-      upVotes: {
-        type: Sequelize.INTEGER,
-      },
-      resolved: {
-        type: Sequelize.BOOLEAN,
-      },
-      solverId: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: "users",
-          key: "id",
-        },
-        onUpdate: "CASCADE",
-        onDelete: "SET NULL",
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("questions");
-  },
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable('questions', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			title: {
+				type: Sequelize.STRING
+			},
+			body: {
+				type: Sequelize.TEXT
+			},
+			authorId: {
+				type: Sequelize.INTEGER,
+				references: {
+					model: 'users',
+					key: 'id'
+				},
+				onUpdate: 'CASCADE',
+				onDelete: 'SET NULL'
+			},
+			questionBoardId: {
+				type: Sequelize.INTEGER,
+				references: {
+					model: 'questionBoards',
+					key: 'id'
+				},
+				onUpdate: 'CASCADE',
+				onDelete: 'SET NULL'
+			},
+			upVotes: {
+				type: Sequelize.INTEGER
+			},
+			resolved: {
+				type: Sequelize.BOOLEAN
+			},
+			solverId: {
+				type: Sequelize.INTEGER,
+				references: {
+					model: 'users',
+					key: 'id'
+				},
+				onUpdate: 'CASCADE',
+				onDelete: 'SET NULL'
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('questions');
+	}
 };
--- models/comment.js
+++ models/comment.js
@@ -1,29 +1,29 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class comment extends Model {
-    /**
+	class comment extends Model {
+		/**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      comment.belongsTo(models.question);
-      comment.belongsTo(models.user);
-    }
-  }
-  comment.init(
-    {
-      questionId: DataTypes.INTEGER,
-      body: DataTypes.TEXT,
-      authorId: DataTypes.INTEGER,
-      upVotes: DataTypes.INTEGER,
-      isSolution: DataTypes.BOOLEAN,
-    },
-    {
-      sequelize,
-      modelName: "comment",
-    }
-  );
-  return comment;
+		static associate(models) {
+			comment.belongsTo(models.question);
+			comment.belongsTo(models.user, { foreignKey: 'authorId', as: 'author' });
+		}
+	}
+	comment.init(
+		{
+			questionId: DataTypes.INTEGER,
+			body: DataTypes.TEXT,
+			authorId: DataTypes.INTEGER,
+			upVotes: DataTypes.INTEGER,
+			isSolution: DataTypes.BOOLEAN
+		},
+		{
+			sequelize,
+			modelName: 'comment'
+		}
+	);
+	return comment;
 };
