acf5a659435f205d894787e27de2c5bea26948cd
RDOToole89 <roibinotoole@gmail.com>
2020-12-09 17:43:41 +0100

    setup SocketIo, setup a socket route to create a new comment

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-const cors = require("cors");
-const express = require("express");
-const { cloudinary } = require("./config/cloudinary");
-const authRouter = require("./routers/auth");
-const boardsRouter = require("./routers/questionBoards");
-const questionsRouter = require("./routers/questions");
+const cors = require('cors');
+const express = require('express');
+const authRouter = require('./routers/auth');
+const boardsRouter = require('./routers/questionBoards');
+const questionsRouter = require('./routers/questions');
+const Comment = require('./models').comment;
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // MiddleWares
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
-app.get("/", (req, res, next) => {
-  res.json("Hello World");
+//SOCKETS IO TEST
+
+const http = require('http');
+const server = http.createServer(app);
+const socket = require('socket.io');
+const io = socket(server, {
+	cors: {
+		origin: 'http://localhost:3000',
+		methods: [ 'GET', 'POST' ],
+		credentials: true
+	}
+});
+
+io.on('connection', (socket) => {
+	socket.emit('your id', socket.id);
+	socket.on('send message', (body) => {
+		io.emit('message', body);
+	});
+
+	socket.on('comment', async (body) => {
+		io.emit('comment', body);
+		console.log('SOCKET.IO FRONTEND BODY', body);
+
+		const newComment = await Comment.create({ ...body });
+		console.log(newComment.dataValues);
+	});
+});
+
+app.get('/', (req, res, next) => {
+	res.json('Hello World');
 });
 
 // Routers
-app.use("/", authRouter);
-app.use("/boards", boardsRouter);
-app.use("/questions", questionsRouter);
+app.use('/', authRouter);
+app.use('/boards', boardsRouter);
+app.use('/questions', questionsRouter);
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
