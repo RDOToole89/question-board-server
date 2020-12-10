79c3be5b2b8fe33b04c6b24a8cf6dd88127b2c48
RDOToole89 <roibinotoole@gmail.com>
2020-12-10 12:22:19 +0100

    Update comments and question seeds to have set dates

--- seeders/20201207154233-some-comments.js
+++ seeders/20201207154233-some-comments.js
@@ -1,66 +1,66 @@
 'use strict';
 
 module.exports = {
-	up: async (queryInterface, Sequelize) => {
-		await queryInterface.bulkInsert(
-			'comments',
-			[
-				{
-					questionId: 1,
-					body:
-						'Spicy jalapeno bacon ipsum dolor amet cow short loin ribeye porchetta filet mignon shank.',
-					authorId: 1,
-					upVotes: 5,
-					isSolution: false,
-					createdAt: new Date(),
-					updatedAt: new Date()
-				},
-				{
-					questionId: 1,
-					body:
-						'Spicy jalapeno bacon ipsum dolor amet cow short loin ribeye porchetta filet mignon shank.',
-					authorId: 1,
-					upVotes: 7,
-					isSolution: false,
-					createdAt: new Date(),
-					updatedAt: new Date()
-				},
-				{
-					questionId: 1,
-					body:
-						'Spicy jalapeno bacon ipsum dolor amet cow short loin ribeye porchetta filet mignon shank.',
-					authorId: 1,
-					upVotes: 10,
-					isSolution: true,
-					createdAt: new Date(),
-					updatedAt: new Date()
-				},
-				{
-					questionId: 2,
-					body:
-						'Spicy jalapeno bacon ipsum dolor amet cow short loin ribeye porchetta filet mignon shank.',
-					authorId: 1,
-					upVotes: 8,
-					isSolution: false,
-					createdAt: new Date(),
-					updatedAt: new Date()
-				},
-				{
-					questionId: 2,
-					body:
-						'Spicy jalapeno bacon ipsum dolor amet cow short loin ribeye porchetta filet mignon shank.',
-					authorId: 1,
-					upVotes: 4,
-					isSolution: false,
-					createdAt: new Date(),
-					updatedAt: new Date()
-				}
-			],
-			{}
-		);
-	},
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'comments',
+      [
+        {
+          questionId: 1,
+          body:
+            'Spicy jalapeno bacon ipsum dolor amet cow short loin ribeye porchetta filet mignon shank.',
+          authorId: 1,
+          upVotes: 5,
+          isSolution: false,
+          createdAt: new Date('December 10, 2020 12:18:30'),
+          updatedAt: new Date(),
+        },
+        {
+          questionId: 1,
+          body:
+            'Spicy jalapeno bacon ipsum dolor amet cow short loin ribeye porchetta filet mignon shank.',
+          authorId: 1,
+          upVotes: 7,
+          isSolution: false,
+          createdAt: new Date('December 10, 2020 12:14:30'),
+          updatedAt: new Date(),
+        },
+        {
+          questionId: 1,
+          body:
+            'Spicy jalapeno bacon ipsum dolor amet cow short loin ribeye porchetta filet mignon shank.',
+          authorId: 1,
+          upVotes: 10,
+          isSolution: true,
+          createdAt: new Date('December 10, 2020 12:13:30'),
+          updatedAt: new Date(),
+        },
+        {
+          questionId: 2,
+          body:
+            'Spicy jalapeno bacon ipsum dolor amet cow short loin ribeye porchetta filet mignon shank.',
+          authorId: 1,
+          upVotes: 8,
+          isSolution: false,
+          createdAt: new Date('December 10, 2020 12:12:30'),
+          updatedAt: new Date(),
+        },
+        {
+          questionId: 2,
+          body:
+            'Spicy jalapeno bacon ipsum dolor amet cow short loin ribeye porchetta filet mignon shank.',
+          authorId: 1,
+          upVotes: 4,
+          isSolution: false,
+          createdAt: new Date('December 10, 2020 12:10:30'),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
 
-	down: async (queryInterface, Sequelize) => {
-		await queryInterface.bulkDelete('comments', null, {});
-	}
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('comments', null, {});
+  },
 };
