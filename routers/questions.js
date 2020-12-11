502866a498d1b5964a6830e98cd599cc2ef7e29b
RDOToole89 <roibinotoole@gmail.com>
2020-12-11 16:20:34 +0100

    Create ROUTE to update comments

--- routers/questions.js
+++ routers/questions.js
@@ -1,16 +1,16 @@
-const { Router } = require("express");
-const { Op } = require("sequelize");
-const authMiddleware = require("../auth/middleware");
-const { cloudinary } = require("../config/cloudinary");
-const User = require("../models").user;
-const Question = require("../models").question;
-const Comment = require("../models").comment;
-const Tag = require("../models").tag;
-const QuestionTag = require("../models").questionTag;
+const { Router } = require('express');
+const { Op } = require('sequelize');
+const authMiddleware = require('../auth/middleware');
+const { cloudinary } = require('../config/cloudinary');
+const User = require('../models').user;
+const Question = require('../models').question;
+const Comment = require('../models').comment;
+const Tag = require('../models').tag;
+const QuestionTag = require('../models').questionTag;
 
 const router = new Router();
 
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const allQuestions = await Question.findAll();
     res.send(allQuestions);
@@ -19,20 +19,20 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.get("/unresolved", async (req, res, next) => {
+router.get('/unresolved', async (req, res, next) => {
   try {
     const allQuestions = await Question.findAll({
       where: { resolved: false },
       include: [
         {
           model: User,
-          as: "author",
-          attributes: ["id", "firstName", "lastName", "classNo"],
+          as: 'author',
+          attributes: ['id', 'firstName', 'lastName', 'classNo'],
         },
         {
           model: User,
-          as: "solver",
-          attributes: ["id", "firstName", "lastName", "classNo"],
+          as: 'solver',
+          attributes: ['id', 'firstName', 'lastName', 'classNo'],
         },
       ],
     });
@@ -41,14 +41,14 @@ router.get("/unresolved", async (req, res, next) => {
     next(error);
   }
 });
-router.get("/queue", async (req, res, next) => {
+router.get('/queue', async (req, res, next) => {
   try {
     const allQuestions = await Question.findAll({
       where: { resolved: false, solverId: null },
       include: {
         model: User,
-        as: "author",
-        attributes: ["id", "firstName", "lastName", "classNo"],
+        as: 'author',
+        attributes: ['id', 'firstName', 'lastName', 'classNo'],
       },
     });
     res.send(allQuestions);
@@ -58,7 +58,7 @@ router.get("/queue", async (req, res, next) => {
 });
 
 //  Route to find a question by id with comments
-router.get("/:id", async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   const { id } = req.params;
 
   try {
@@ -67,18 +67,16 @@ router.get("/:id", async (req, res, next) => {
       include: [
         {
           model: User,
-          as: "author",
-          attributes: ["id", "firstName", "lastName", "classNo"],
+          as: 'author',
+          attributes: ['id', 'firstName', 'lastName', 'classNo'],
         },
-        { model: Comment, include: [{ model: User, as: "author" }] },
-        { model: Tag, attributes: ["id", "tagname", "createdAt"] },
+        { model: Comment, include: [{ model: User, as: 'author' }] },
+        { model: Tag, attributes: ['id', 'tagname', 'createdAt'] },
       ],
     });
 
     if (!question) {
-      res
-        .status(400)
-        .send({ message: "Something went wrong, question not found" });
+      res.status(400).send({ message: 'Something went wrong, question not found' });
       return;
     }
 
@@ -89,7 +87,7 @@ router.get("/:id", async (req, res, next) => {
 });
 
 // Route icrements question upvotes
-router.put("/upvote/:id", async (req, res, next) => {
+router.put('/upvote/:id', async (req, res, next) => {
   const { id } = req.params;
 
   try {
@@ -111,35 +109,55 @@ router.put("/upvote/:id", async (req, res, next) => {
   }
 });
 
-router.put(
-  "/:questionid/comments/:commentid/upvotes",
-  async (req, res, next) => {
-    const { questionid, commentid } = req.params;
+router.put('/:questionid/comments/:commentid/upvotes', async (req, res, next) => {
+  const { questionid, commentid } = req.params;
 
-    try {
-      const commentToUpdate = await Comment.findByPk(commentid);
+  try {
+    const commentToUpdate = await Comment.findByPk(commentid);
 
-      if (commentToUpdate) {
-        try {
-          const updatedComment = await commentToUpdate.update({
-            ...commentToUpdate,
-            upVotes: (commentToUpdate.upVotes += 1),
-          });
+    if (commentToUpdate) {
+      try {
+        const updatedComment = await commentToUpdate.update({
+          ...commentToUpdate,
+          upVotes: (commentToUpdate.upVotes += 1),
+        });
 
-          if (updatedComment) {
-            return res.status(200).json(updatedComment);
-          }
-        } catch (e) {
-          next(e);
+        if (updatedComment) {
+          return res.status(200).json(updatedComment);
         }
+      } catch (e) {
+        next(e);
       }
-    } catch (e) {
-      next(e);
     }
+  } catch (e) {
+    next(e);
   }
-);
+});
 
-router.put("/:id", authMiddleware, async (req, res, next) => {
+router.put('/:questionid/comments/:commentid/', authMiddleware, async (req, res, next) => {
+  const { questionid, commentid } = req.params;
+  const { key, newValue } = req.body;
+
+  try {
+    const commentToUpdate = await Comment.findByPk(commentid);
+    if (commentToUpdate) {
+      try {
+        const updatedComment = await commentToUpdate.update({
+          ...commentToUpdate,
+          [key]: newValue,
+        });
+
+        res.json(updatedComment);
+      } catch (e) {
+        next(e);
+      }
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put('/:id', authMiddleware, async (req, res, next) => {
   const { id } = req.params;
   const { key, newValue } = req.body;
 
@@ -161,14 +179,14 @@ router.put("/:id", authMiddleware, async (req, res, next) => {
     next(e);
   }
 });
-router.post("/", authMiddleware, async (req, res, next) => {
+router.post('/', authMiddleware, async (req, res, next) => {
   try {
     const authorId = req.user.id;
     const { image, title, questionBoardId, body, tags } = req.body;
     let screenshotURL;
     if (image) {
       const uploadedResponse = await cloudinary.uploader.upload(image, {
-        upload_preset: "ml_default",
+        upload_preset: 'ml_default',
       });
       screenshotURL = uploadedResponse.url;
     }
@@ -194,9 +212,7 @@ router.post("/", authMiddleware, async (req, res, next) => {
     });
 
     tags.forEach(async (tag) => {
-      const tagExist = allTags.find(
-        (tagfromArray) => tagfromArray.tagname === tag
-      );
+      const tagExist = allTags.find((tagfromArray) => tagfromArray.tagname === tag);
       if (tagExist) {
         const newQuestionTag = await QuestionTag.create({
           questionId: newQuestion.id,
