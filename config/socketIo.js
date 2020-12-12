59e2df81c6cae2cf3ca34d8816fd006cee261e02
RDOToole89 <roibinotoole@gmail.com>
2020-12-12 16:17:57 +0100

    Comment out redundent socket.io file

--- config/socketIo.js
+++ config/socketIo.js
@@ -1,10 +1,10 @@
-const socket = require('socket.io');
-const io = socket(server, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-    credentials: true,
-  },
-});
+// const socket = require('socket.io');
+// const io = socket(server, {
+//   cors: {
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST'],
+//     credentials: true,
+//   },
+// });
 
-module.exports = io;
+// module.exports = io;
